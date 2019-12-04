fdf9ee9681584290355f224ec00c0728ca59c995
Zuzana Kůtová <zuzana.kutova@seznam.cz>
2019-12-04 20:44:28 +0100

    center todolist title

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,7 +6,7 @@ export default class TodoList extends Component {
         return (
 
             <ul className="list-group my-5">
-                <h3 className="text-capitalize">todo list</h3>
+                <h3 className="text-capitalize text-center">todo list</h3>
                 {items.map(item => {
                     return (
                         <TodoItem
